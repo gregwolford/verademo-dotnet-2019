4db83a45b92bdd8accb619ea3dd46b88ae731f33
Greg Wolford <gregnwolford@gmail.com>
2021-10-06 11:11:24 -0500

    first commit

---
+++ Verademo-Dotnet/XssInjection.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Verademo_dotnet
+{
+    public partial class XssInjection : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["text"] != null)
+            {
+                // Response.Write(Request.QueryString["text"]);
+
+                Response.Write(System.Net.WebUtility.HtmlEncode(Request.QueryString["text"]));
+            }
+        }
+    }
+}
---
+++ Verademo-Dotnet/obj/Debug/AspnetCompileMerge/Source/SqlInjection.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SqlInjection.aspx.cs" Inherits="Verademo_dotnet.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="FormSqlInjectionExample" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
